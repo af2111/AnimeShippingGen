0f23550c5b89f18bda17b9977715bc75157a1661
af2111 <af@martinfritz.net>
2020-09-02 09:56:10 +0200

    revamped the code and added comments

--- Program.cs
+++ Program.cs
@@ -2,52 +2,56 @@
 using System.IO;
 namespace AnimeShippingGen
 {
-    class AnimeCharakter{
-        public String name;
-        public String animeTitel;
-        
-        public AnimeCharakter(String name2, String animeTitel2) {
-            this.name = name2;
-            this.animeTitel = animeTitel2; 
-        }
-        public String toString() {
-            return $"{name}({animeTitel})";
-        }
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            String path = "textfile.txt";
-            string[] lines = File.ReadAllLines(path);  
-            int linesCount = lines.Length;  
-            AnimeCharakter[] charaktere = new AnimeCharakter[linesCount];
-            for(int i = 0; i < linesCount; i++) {
-                String words = lines[i];
-                String[] splitWords = words.Split(", ");
-                AnimeCharakter a = new AnimeCharakter(splitWords[0], splitWords[1]);
-                charaktere[i] = a;
+        
+        //define path for input and output file
+        String readFile = "input.txt";
+        String writeFile = "output.txt";
+        //create file if it doesn't exist
+        if(!File.Exists(readFile)) {
+        FileStream fs = File.Create(readFile);
+        }
+        //some predefined characters from My Hero Academia
+        String[] preCharacters = {"Ochako, MHA", "Deku, MHA", "Bakugo, MHA", "Shoto, MHA"};
+        //add precharacters if file is empty
+        if(File.ReadAllLines(readFile).Length == 0) {
+            System.IO.File.WriteAllLines(readFile, preCharacters);
+        }
+        //read all lines
+        String[] lines = File.ReadAllLines(readFile);
+
+        
+       
+
+        //format the input from the text file
+        String[] characters = new String[lines.Length];
+        for(int i = 0; i < lines.Length; i++) {
+            String[] words = lines[i].Split(", ");
+            characters[i] = $"{words[0]}({words[1]})";
+        }
+        
+        Random rand = new Random();
+        int randomNumber1 = rand.Next(lines.Length - 1);
+        Random rand2 = new Random(rand.Next(110223));
+        int randomNumber2 = rand2.Next(lines.Length - 1);
+        while(randomNumber1 == randomNumber2) {
+               randomNumber1 = rand.Next(lines.Length - 1);
+               randomNumber2 = rand2.Next(lines.Length - 1);
             }
-            var rand = new Random();
-            var rand2 = new Random(rand.Next(232323));
-            var randomNumber1 = rand.Next(linesCount);
-            var randomNumber2 = rand2.Next(linesCount);
-            while(randomNumber1 == randomNumber2) {
-                randomNumber1 = rand.Next(linesCount);
-                randomNumber2 = rand2.Next(linesCount);
-            }
-            
-            using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(@"output.txt", true))
+        using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(writeFile, true))
         {
-            file.WriteLine(charaktere[randomNumber1].toString() + " x " + charaktere[randomNumber2].toString());
+            file.WriteLine(characters[randomNumber1] + " x " + characters[randomNumber2]);
         }
 
-    }
 
 
             
-            
         }
     }
+}
 
